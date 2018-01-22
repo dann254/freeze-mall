96dd88990805ea5142efc558da947ee0d95f017c
Daniel Kipkemei <daniel.kipkemei@andela.com>
2018-01-22 10:49:08 +0300

    [FEATURE 154309878] Production database URL

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_deliveries = true
 
-  config.action_mailer.default_url_options = { host: ENV["HOST"], port: ENV["PORT"] }
+  config.action_mailer.default_url_options = { host: ENV["E_HOST"], port: ENV["E_PORT"] }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.smtp_settings = {
